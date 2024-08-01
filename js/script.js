6d305342944885904aa07c6cc0641d410e7e3c91
phbogner <phlpbgnr@gmail.com>
2024-08-01 14:04:12 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -57,7 +57,7 @@ function initApp() {
       this.setFirstTime(false);
     },
         async menutest() {
-      const response = await fetch("https://phbogner.github.io/SimplePOS-Live/data/sample.json");
+      const response = await fetch("data/sample.json");
       const data = await response.json();
       this.products = data.products;
       for (let product of data.products) {
